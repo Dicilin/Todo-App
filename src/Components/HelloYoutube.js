4781848dce65eda231c04133ffec6e35998b6956
Dicilin <dicilin15@gmail.com>
2024-03-20 02:44:02 +0800

    deploy

---
+++ src/Components/HelloYoutube.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Des({Fname, Lname, Email}){
+    return(
+        <div>
+       <h1>Firstname: {Fname}</h1> 
+       <h1>Lastname: {Lname}</h1> 
+       <h1>Email: {Email}</h1> 
+        </div>
+    );
+}
+
+
